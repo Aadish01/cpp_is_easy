3ee4283f5b1c98bfa4b90797b39927b4964d64f0
Aadish Jain <aadish.dev01@gmail.com>
2022-10-24 17:13:03 +0530

    commit

---
+++ Recursion/mast questions/CoinChangeWithoutDP.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int coin_change(int sum,int *coins,int num_coins, int pre)
+{
+    if(sum<0)
+        return 0 ;
+    if(sum==0)
+        return 1 ;
+    int all = 0 ;
+    for(int i=0;i<num_coins;i++)
+    {
+        if(coins[i]>=pre) 
+            all += coin_change(sum-coins[i], coins, num_coins, coins[i]) ;
+    }
+    return all ;
+}
+int main()
+{
+    int coins[] = {1,2,3} ;
+    cout << coin_change(6, coins, 3, 0) ;
+    // 4th args is same as 1st
+    // 5 is always zero
+    return 0;
+}
+// 4: 1111 112 13 22 
+// 5: 11111 1112 122 113 23 
+// 6: 111111 11112 1113 1122 123 222 33
+// 7: 1111111 111112 11113 11122 1123 1222 133 223 
