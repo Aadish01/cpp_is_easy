ed28a1e66886877dd2b468819216704e262ecc79
Aadish Jain <aadish.dev01@gmail.com>
2023-01-26 15:29:37 +0530

    commit

---
+++ DataStructure/Stack/StackUsingTemplate.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+template<typename T>
+class Stack
+{
+    T *data ;
+    int size ;
+    int nextIndex ;
+
+    public:
+
+    Stack(int size)
+    {
+        this -> size = size ;
+        nextIndex = 0 ;
+        data = new T[size] ;
+    }
+    bool IsEmpty()
+    {
+        return nextIndex == 0 ;
+    }
+    T Size()
+    {
+        return nextIndex ; 
+    }
+    bool IsFull()
+    {
+        return nextIndex == size ;
+    }
+    void push(T i)
+    {
+        if(!IsFull())
+        {
+            data[nextIndex] = i ;
+            nextIndex ++ ;
+        }
+        else
+            cout << "Stack full...\n" ;
+    }
+    T pop()
+    {
+        if(!IsEmpty())
+        {        
+            nextIndex -- ;
+            return data[nextIndex] ;
+        }
+        else
+            return INT_MIN ;
+    }
+    T top()
+    {
+        if(!IsEmpty())
+            return data[nextIndex-1] ;
+        else 
+            return INT_MIN  ;
+    }
+    void print()
+    {
+        int i = 0 ;
+        while(i<nextIndex)
+        {
+            cout << data[i] << " " ;
+            ++i ;
+        }
+        cout << "\n" ;
+    }
+};
+
